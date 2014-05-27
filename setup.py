c8344e0728f87ad5e2f315a3cca64dffbb1816f0
Michal Czaplinski <mmczaplinski@gmail.com>
2014-05-27 21:47:21 +0200

    added database

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='digital_graveyard',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='just an app',
+      author='Michal Czaplinski',
+      author_email='mmczaplinski@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask>=0.9','flask-wtf','flask-babel','markdown','flup','MarkupSafe'],
      )
