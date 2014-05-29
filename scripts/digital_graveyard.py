46ea1927f640a8ece4606d13bc31b6ca94a952e3
Michal Czaplinski <mmczaplinski@gmail.com>
2014-05-29 20:14:56 +0200

    deleted jquery, added textfiles, moved the datbase

--- scripts/digital_graveyard.py
+++ scripts/digital_graveyard.py
@@ -1,8 +1,7 @@
-import twitter, re, time, logging, os.path, unicodedata, arduino_printer
-
+import TwitterAPI, re, time, logging, os.path, unicodedata, sqlite3, os
+from Queue import Queue
 
 ##  Logging
-
 logger = logging.getLogger("__main__")
 logger.setLevel(logging.DEBUG)
 
@@ -29,38 +28,38 @@ def get_names_from_files():
     return first_names, last_names, honorifics
 
 
+def connect_to_streaming_API():
+    ''' Connect to twitter streaming API
+        Returns an iterator with all the stream data. '''
 
-def connect_to_API_and_get_statuses():
-    ''' Connect to twitter API and pull tweets (statuses) '''
+    api = TwitterAPI.TwitterAPI(
+            'f8olfZWtAPvgANdP9qecg',
+            'bSEnCXJuWazjT8S8hZ6BLWMo1C7egIKNgjObHM6Ck',
+            '1726636778-jEn4qUAj2wV60ckbskNSbLJgTRr0c7hiemVOU7x',
+            'UgwEfM3cukoWIxCWjCiIZiJ0gnQVGH9U42WLfJjnEFODw')
 
-    api = twitter.Api(consumer_key='f8olfZWtAPvgANdP9qecg',
-                        consumer_secret='bSEnCXJuWazjT8S8hZ6BLWMo1C7egIKNgjObHM6Ck',
-                        access_token_key='1726636778-jEn4qUAj2wV60ckbskNSbLJgTRr0c7hiemVOU7x',
-                        access_token_secret='UgwEfM3cukoWIxCWjCiIZiJ0gnQVGH9U42WLfJjnEFODw')
-    statuses = api.GetSearch(term='#rip', count=100, result_type='recent')
-    return statuses
+    r = api.request('statuses/filter', {'track': '#rip'})
 
+    return r.get_iterator()
 
 
 def is_retweet(text):
     ''' Checks if the text starts with RT (indicating a retweet)  '''
-
     if text.startswith('RT'):
         return True
     else:
         return False
 
 
-
-def clean_up_tweet_text(text):
+def remove_user_mentions(text):
     ''' Removes user mentions from the tweet text '''
 
     text = re.sub('\s@[a-zA-Z_]*', ' ', text)
     return text
 
 
-
 def clean_up_name(name):
+
     # get rid of newlines
     name = name.rstrip()
 
@@ -71,28 +70,17 @@ def clean_up_name(name):
     # get rid of some punctuation
     name = name.strip('()",.! ')
 
-    # catch names ending with  's
+    # catch names ending with 's
     if name.endswith('\'s'):
         name.rstrip('\'s')
 
     return name
 
 
-
-def is_name_in_text(name, text):
-    ''' Checks if the string contains the name '''
-
-    if re.search('\s+' + name + '\s', text) is not None:
-        return True
-    else:
-        return False
-
-
-
 def precheck_text(text):
     ''' Checks the tweet text for a number of patterns
-        before checking for a name from the list
-    '''
+        before checking for a name from the list.
+        Currently checking for "my" <something> "passed away", etc. '''
 
     # looks for the "my" <sth> "died" pattern
     for phrase in ['died', 'passed away', 'is dead', 'gone']:
@@ -104,7 +92,6 @@ def precheck_text(text):
                 return name
     else:
         return None
-
 
 
 def get_the_rest(name, text):
@@ -121,19 +108,15 @@ def get_the_rest(name, text):
     return rest_of_text
 
 
-
 def look_for_first_name(text, first_names, honorifics):
     ''' Recursively check every word of the tweet text for the name.
-
         If the text contains a name that is specified in first_names returns the name.
-        Otherwise returns None.
-
-    '''
+        Otherwise returns None. '''
 
     #split the string into two parts at the first occurence of whitespace
     split_text = text.split(None, 1)
 
-    # check if we are on the last word in the
+    # check if we are on the last word
     try:
         remaining_text = split_text[1]
     except IndexError:
@@ -142,7 +125,7 @@ def look_for_first_name(text, first_names, honorifics):
     candidate_name = split_text[0]
     #candidate_name = clean_up_name(candidate_name)
 
-    # if we encounter an honorific, gotta look ahead to the next word, see if it's a name
+    # if we encounter an honorific, look ahead to the next word, see if it's a name
     if candidate_name in honorifics:
 
         second_word = remaining_text.split()[0]
@@ -158,12 +141,9 @@ def look_for_first_name(text, first_names, honorifics):
 
     elif candidate_name in first_names:
         return candidate_name
-
     else:
-
         # Run again for the rest of the tweet, unless it's the end of the tweet.
         return look_for_first_name(remaining_text, first_names, honorifics)
-
 
 
 def get_full_name_from_tweet_text(name, text, name_so_far, first_names, last_names):
@@ -185,98 +165,86 @@ def get_full_name_from_tweet_text(name, text, name_so_far, first_names, last_nam
         return name_so_far
 
 
-
-def get_latest_tweets(include_retweets=True):
+def get_latest_tweets(stream):
     ''' The core function.
-        Returns a 3-tuple: (time of the tweet, the extracted name, the full text of the tweet)
+        Returns a 4-tuple:
+        (time of the tweet, the extracted name, the full text of
+            the tweet and the the username of the person tweeting). '''
 
-    '''
-    statuses = connect_to_API_and_get_statuses()
     first_names, last_names, honorifics = get_names_from_files()
 
-    tweets = []
+    for s in stream:
 
-    for s in statuses:
+        text, time, user = s['text'], s['created_at'], s['user']['screen_name']
 
-        text = s.text
-        time = s.created_at
-        user = s.user.screen_name
+        cleaned_up_text = remove_user_mentions(text)
 
-        cleaned_up_text = clean_up_tweet_text(text)
+        tweet = {'text': text, 'time': time, 'user': user, 'name': None, 'retweet': 0}
 
-        if include_retweets is False:
-            if is_retweet(cleaned_up_text):
-                continue
+        if is_retweet(cleaned_up_text):
+            tweet['retweet'] = 1
 
         # check for the 'My' <somethingsomething> 'died', etc. pattern
-        name = precheck_text(text)
+        name = precheck_text(cleaned_up_text)
         if name is not None:
-            tweets.append((time, name, text))
+            tweet['name'] = name
+            yield tweet
 
-        # checks if there is a first name
+        # find a full name
         name = look_for_first_name(cleaned_up_text, first_names, honorifics)
-
         if name is not None:
             name_from_tweet = get_full_name_from_tweet_text(name, cleaned_up_text, name, first_names, last_names)
-            tweets.append((time, name_from_tweet, text))
-
-    return tweets
-
+            tweet['name'] = name_from_tweet
+            yield tweet
 
 
-def print_tweets(tweets):
-
-    if len(tweets) == 0:
-        time.sleep(100)
-
-    elif len(tweets) > 0:
-        sleep_time = 120/len(tweets)
-        for tweet in tweets:
-            # name is indexed as [1], if you want another attribute  time: [0], tweet text: [2]
-            name = tweet[1]
-            # normalize and change to ascii
-            name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
-            print tweet[0]
-            print tweet[1]
-            print tweet[2], '\n'
-            arduino_printer.show_text(name, sleep_time)
-
-    return
+def save_tweet_to_db(tweet, database):
+    tweet_values = tweet['text'], tweet['user'], tweet['time'], tweet['name'], tweet['retweet']
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    c.execute('INSERT INTO tweet (text, user, time, name, retweet_status) VALUES (?,?,?,?,?)', tweet_values)
+    conn.commit()
+    conn.close()
 
 
+def print_tweet(tweet):
+    # name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
+    print tweet['time'], '\n', tweet['name'], '\n', tweet['text'], '\n', tweet['user'], '\n\n'
 
-def main(old_tweets = []):
-    while True:
 
-        new_tweets = get_latest_tweets()
+def main():
 
-        # x[1] bucause of the (time, name, tweet) tuple
-        tweets = [ x for x in new_tweets if x not in old_tweets ]
-        print_tweets(tweets)
-        old_tweets.extend(new_tweets)
-
+    database = os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), 'tweets.db')
+    stream = connect_to_streaming_API()
+    tweets = get_latest_tweets(stream)
+    for tweet in tweets:
+        save_tweet_to_db(tweet, database)
+        print_tweet(tweet)
 
 
 if __name__ == '__main__':
+    main()
+
+# big_dict = {}
+# for item in items:
+#     text = item[0]
+#     user = item[1]
+
+#     if not item[2]:
+#         big_dict[text] = [user]
+#     elif text not in big_dict:
+#         big_dict[text] = [user]
+#     else:
+#         big_dict[text].append(user)
 
 
-    while True:
-        try:
-            main()
+# look for a more common name w pierwszej kolejnosci (so that if there is a word
+# in the tweet text that is identical to a name the ACTUAL name would get selected)
 
-        except StandardError as e:
-            print 'There was an error, restarting in 3 minutes...'
-            logger.exception(e)
-            time.sleep(180)
+# reconnecting wrapper
+
+# retweet get the same name (same grave)
 
 
-
-    #main()
-
-
-# BUGS AND ISSUES:
-# - improve the find_the_first_word function (put the honorifics functionality in a separate function.)
-# - investigate 'NoneType' object has no attribute 'end'
-# - INCLUDE is_name_in_text function
 
 
